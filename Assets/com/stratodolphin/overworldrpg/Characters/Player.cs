4d842b087d915f6022f260f0ee3180068e508ec2
ianbro <ianmann56@gmail.com>
2017-10-21 15:04:48 -0400

    Started Enemy AI
    I also layed down the growndwork for the game

---
+++ Assets/com/stratodolphin/overworldrpg/Characters/Player.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour
+{
+	/// <summary>
+	/// The game object that represents the main player of
+	/// the game.
+	/// </summary>
+	private GameObject _playerObject;
+
+	/// <summary>
+	/// Accessor for the GameObject that represents this player.
+	/// </summary>
+	/// <value>The player object.</value>
+	public GameObject PlayerObject { get { return _playerObject; } }
+
+	// Use this for initialization
+	void Start ()
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	
+	}
+}
+
---
+++ Assets/com/stratodolphin/overworldrpg/Game.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace AssemblyCSharp
+{
+	public abstract class Game
+	{
+		/// <summary>
+		/// Actual value for the main player global variable. This should
+		/// only be accessed by the Attribute <see cref="Game.MainPlayer"/>
+		/// except by the initialize function.
+		/// </summary>
+		private static Player _mainPlayer;
+
+		/// <summary>
+		/// Accessor for Game._mainPlayer.
+		/// </summary>
+		/// <value>The main player.</value>
+		public static Player MainPlayer { get { return _mainPlayer; } }
+
+		/// <summary>
+		/// Initialize the global variables in this class for the game.
+		/// </summary>
+		public static void initialize() {
+
+		}
+	}
+}
+
