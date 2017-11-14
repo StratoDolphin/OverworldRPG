b6e59bb8d109435b04e79e549ac74703b948fd16
Kirkpatrick <kirkp1ia@cmich.edu>
2017-11-14 09:42:51 -0500

    Merged Anthony's camera stuff

---
+++ Assets/com/stratodolphin/overworldrpg/Characters/Camera/Raycast3.js
@@ -0,0 +1,14 @@
+﻿static var distance3 : float = 5;
+
+function Update () {
+
+	var hit: RaycastHit;
+	if(Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), hit)){
+
+	//print("inside Raycast3, distance3 is:");
+	print(distance3);
+
+	distance3 = hit.distance;
+
+	}
+}
