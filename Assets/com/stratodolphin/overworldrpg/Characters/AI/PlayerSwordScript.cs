4eccab9c16c9b47a4a55579a6201d4d5b1199ce5
Anthony MacKay <macka1aj@cmich.edu>
2017-11-30 01:30:49 -0500

    git player sword

---
+++ Assets/com/stratodolphin/overworldrpg/Characters/AI/PlayerSwordScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerSwordScript : MonoBehaviour {
+	EnemyAI enemyScript;
+	GameObject enemy;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+
+
+	void OnTriggerEnter(Collider other) {
+		Debug.Log ("Sword hit!");
+		Debug.Log ("ON!!!!");
+		Debug.Log (other);
+		enemy = other.transform.parent.gameObject;
+		if (enemy.tag == "AI") {
+			enemyScript = enemy.GetComponent<EnemyAI> ();
+			enemyScript.decreaseHealth ((float)10.0);
+		}
+		//theMainPlayer.decreaseHealth (1.0);
+	}
+}
